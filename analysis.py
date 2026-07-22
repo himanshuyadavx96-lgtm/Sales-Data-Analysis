41840124f8a29e38ae4be7a204c5f1aa2ca437a5
Himanshu Yadav <himanshuyadavx96@gmail.com>
2026-07-22 22:12:08 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("sales_data.csv")
+
+print("Total Sales:", df["Sales"].sum())
+print("Total Profit:", df["Profit"].sum())
+
+product_sales = df.groupby("Product")["Sales"].sum()
+print(product_sales)
+
+product_sales.plot(kind="bar")
+plt.title("Product Wise Sales")
+plt.show()
